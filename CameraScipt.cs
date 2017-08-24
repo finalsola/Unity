7a5610d6cfcb7d6aed6d57b4d028623dfb9a810b
finalsola <122825885@qq.com>
2017-08-24 11:36:15 +0800

    第三人称视角镜头控制
    另一种方法在图中，主要是用

---
+++ CameraScipt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScipt : MonoBehaviour {
+    public Transform target;
+    float h;
+    float v;
+    float hkey;
+    float vkey;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+        h = Input.GetAxis("Mouse X");
+        v = Input.GetAxis("Mouse Y");
+        hkey = Input.GetAxis("Horizontal");
+        vkey = Input.GetAxis("Vertical");
+
+        transform.RotateAround(target.position, transform.right, -v);
+        transform.RotateAround(target.position, transform.up, h);
+
+        transform.Translate(target.TransformDirection(new Vector3(hkey, 0, vkey)), Space.World);
+    }
+}
---
+++ move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    float hkey;
+    float vkey;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hkey = Input.GetAxis("Horizontal");
+        vkey = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(hkey, 0, vkey));
+    }
+}
