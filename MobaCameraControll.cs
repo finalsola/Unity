42fef7435c042a05891e84b349db2487f0ddc5c0
finalsola <122825885@qq.com>
2017-08-31 19:56:12 +0800

    Moba类摄像头控制，脚本挂在一个空Gameobject上，摄像机是这个Gameobject的child

---
+++ MobaCameraControll.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour {
+    Vector3 mousePosition;
+    float x;
+    float y;
+    public float speed = 5;
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        mousScreenMove();
+
+    }
+
+    void mousScreenMove()
+    {
+        x = 0;
+        y = 0;
+
+        if(Input.mousePosition.x<0||Input.mousePosition.x>Screen.width)
+		{
+            x = Input.mousePosition.x / Mathf.Abs(Input.mousePosition.x);
+            y = -Screen.height / 2 + Input.mousePosition.y;
+            y = 2*y/Screen.height;
+        }
+        else if (Input.mousePosition.y < 0 || Input.mousePosition.y > Screen.height)
+		{
+            y = Input.mousePosition.y / Mathf.Abs(Input.mousePosition.y);
+            x =- Screen.width / 2 +Input.mousePosition.x;
+            x = 2 *x/ Screen.width;
+		}
+        x = Mathf.Clamp(x,-1, 1);
+        y = Mathf.Clamp(y, -1, 1);
+
+        transform.Translate(new Vector3(x*Time.deltaTime*speed,0,y*Time.deltaTime*speed));
+    }
+}
