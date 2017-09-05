ec8e21cf1f39d72fd3da65c3cb2814f6b69b0248
finalsola <122825885@qq.com>
2017-09-05 10:26:33 +0800

    平滑移动和旋转

---
+++ LerpMoveandRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpMoveandRotate : MonoBehaviour {
+    Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //平滑移动
+        transform.position = Vector3.Lerp(transform.position, target.position,1/Vector3.Distance(transform.position,target.position));
+        //平滑旋转
+        Quaternion targetQ = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetQ, 1 / Quaternion.Angle(transform.rotation, targetQ) * 0.1f);
+
+	}
+}
