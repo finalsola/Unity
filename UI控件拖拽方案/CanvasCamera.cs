bf94e4eade7e72058f55cefe1d8b274ca34bff04
finalsola <122825885@qq.com>
2017-09-14 10:33:53 +0800

    UI控件拖拽方案
    两种Render Mode

---
+++ UI控件拖拽方案/CanvasCamera.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Canvas2 : MonoBehaviour,IDragHandler {
+	RectTransform rectTransform;
+	Camera mainCamera;
+	RectTransform canvas;
+	Vector3 mousepos;
+    Canvas canvasScript;
+    float canvasDepth;
+    // Use this for initialization
+    void Start()
+    {
+        rectTransform = transform as RectTransform;
+        mainCamera = Camera.main;
+        canvas = transform.parent as RectTransform;
+        canvasScript = canvas.GetComponent<Canvas>();
+        canvasDepth = canvasScript.planeDistance;
+    }
+
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+	public void OnDrag(PointerEventData eventData)
+	{
+        mousepos = Input.mousePosition;
+        mousepos = mainCamera.ScreenToWorldPoint(new Vector3(mousepos.x, mousepos.y, canvasDepth));
+        rectTransform.position = mousepos;
+   //     if (transform.localPosition.x > canvas.rect.width / 2-rectTransform.rect.width/2)
+   //         transform.localPosition = new Vector3(canvas.rect.width / 2- rectTransform.rect.width / 2, transform.localPosition.y, 0);
+   //     else if(transform.localPosition.x<-canvas.rect.width/2+ rectTransform.rect.width / 2)
+			//transform.localPosition = new Vector3(-canvas.rect.width / 2+ rectTransform.rect.width / 2, transform.localPosition.y, 0);
+        //if(transform.localPosition.y>canvas.rect.height/2-rectTransform.rect.height/2)
+        //    transform.localPosition = new Vector3(transform.localPosition.x, canvas.rect.height/2- rectTransform.rect.height / 2, 0);
+        //else if(transform.localPosition.y < -canvas.rect.height / 2+rectTransform.rect.height / 2)
+            //transform.localPosition = new Vector3(transform.localPosition.x, -canvas.rect.height / 2+ rectTransform.rect.height / 2, 0);
+        //避免出界；
+        rectTransform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x, -canvas.rect.width / 2 + rectTransform.rect.width / 2, canvas.rect.width / 2 - rectTransform.rect.width / 2),
+                                                Mathf.Clamp(transform.localPosition.y, -canvas.rect.height / 2 + rectTransform.rect.height / 2, canvas.rect.height / 2 - rectTransform.rect.height / 2));
+	}
+}
---
+++ UI控件拖拽方案/CanvasOverlay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Canvas1 : MonoBehaviour ,IDragHandler{
+    RectTransform rectTransform;
+    Camera mainCamera;
+    RectTransform canvas;
+    Vector3 mousepos;
+    CanvasScaler canvasscaler;
+	// Use this for initialization
+	void Start () {
+        rectTransform = transform as RectTransform;
+        mainCamera = Camera.main;
+        canvas = transform.parent as RectTransform;
+        canvasscaler = canvas.GetComponent<CanvasScaler>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        mousepos = Input.mousePosition;
+        rectTransform.anchoredPosition = new Vector2(mousepos.x * canvas.rect.width / Screen.width, mousepos.y * canvas.rect.height / Screen.height);
+        //更快速的方法
+        //transform.position = Input.mousePosition / canvasscaler.scaleFactor;
+    }
+}
